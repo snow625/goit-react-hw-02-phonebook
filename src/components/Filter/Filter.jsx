d38fc9afe075fd1e3014204f8fdffc1c1af7e3a9
Dmitriy Narishkin <97532271+snow625@users.noreply.github.com>
2022-06-30 17:16:20 +0300

    fix function

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,24 +1,19 @@
-import { Component } from "react";
-
 import PropTypes from "prop-types";
 import s from "./filter.module.css";
-class Filter extends Component {
-  render() {
-    const { onChange } = this.props;
-    return (
-      <label className={s.label}>
-        Find contacts by name
-        <input
-          onChange={onChange}
-          className={s.input}
-          placeholder="enter name"
-          type="text"
-          name="filter"
-        />
-      </label>
-    );
-  }
-}
+const Filter = ({ onChange }) => {
+  return (
+    <label className={s.label}>
+      Find contacts by name
+      <input
+        onChange={onChange}
+        className={s.input}
+        placeholder="enter name"
+        type="text"
+        name="filter"
+      />
+    </label>
+  );
+};
 
 Filter.propTypes = {
   onChange: PropTypes.func.isRequired,
